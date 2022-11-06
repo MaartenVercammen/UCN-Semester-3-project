1adb70b19fe3774de739e09ca96eeaa857a78174
MaartenVercammen <vercammen.maarten.2000@gmail.com>
2022-11-06 20:53:22 +0100

    Make changes so docker work and add docker compose for whole project

--- backend/UCN-Semester-3-project/RecipeRestService/Program.cs
+++ backend/UCN-Semester-3-project/RecipeRestService/Program.cs
@@ -10,13 +10,10 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
-app.UseHttpsRedirection();
+// app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
