41e95f472d65c377b1af5019edf96d2ef4c17ee4
MaartenVercammen <vercammen.maarten.2000@gmail.com>
2022-11-09 12:51:37 +0100

    fix swagger

--- backend/UCN-Semester-3-project/RecipeRestService/Controllers/WeatherForecastController.cs
+++ backend/UCN-Semester-3-project/RecipeRestService/Controllers/WeatherForecastController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace WebApplication1.Controllers
 {
     [ApiController]
-    [Route("api/[controller]")]
+    [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
