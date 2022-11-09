4a54a58efea23a37c69dd9166797214ef3ac4551
MaartenVercammen <91468225+MaartenVercammen@users.noreply.github.com>
2022-11-09 12:48:44 +0100

    Dev (#14)
    * Feature/add docker (#1)
    
    * Add docker file
    
    * add dockerfile
    
    * Feature/add docker (#2)
    
    * Add docker file
    
    * add dockerfile
    
    * Make changes so docker work and add docker compose for whole project
    
    * some added changes
    
    * Feature/add docker (#4)
    
    * Add docker file
    
    * add dockerfile
    
    * Make changes so docker work and add docker compose for whole project
    
    * some added changes
    
    * update dockerignore file location
    
    * Feature/add docker (#6)
    
    * Add docker file
    
    * add dockerfile
    
    * Make changes so docker work and add docker compose for whole project
    
    * some added changes
    
    * update dockerignore file location
    
    * update gitignore for backend
    
    * move dockerfile
    
    * fix extra file
    
    * updated gitignore
    
    * fix footer
    
    * fix swagger calls
    
    * fix update
    
    * fix swagger
    
    Co-authored-by: Tomáš Sabol <tomassabol2@gmail.com>

--- backend/UCN-Semester-3-project/RecipeRestService/Controllers/WeatherForecastController.cs
+++ backend/UCN-Semester-3-project/RecipeRestService/Controllers/WeatherForecastController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace WebApplication1.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/[controller]")]
     public class WeatherForecastController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
--- backend/UCN-Semester-3-project/RecipeRestService/Program.cs
+++ backend/UCN-Semester-3-project/RecipeRestService/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.Extensions.Options;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
