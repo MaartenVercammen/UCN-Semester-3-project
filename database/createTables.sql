0a23c519690c01ab10d2b38aeddbac13ea70f0bb
Tomáš Sabol <tomassabol2@gmail.com>
2022-11-10 11:34:27 +0100

    sql create script

---
+++ database/createTables.sql
@@ -0,0 +1,84 @@
+use ucn;
+
+drop table if exists bambooSessionUser;
+drop table if exists bambooSession;
+drop table if exists list;
+drop table if exists ingredient;
+drop table if exists instruction;
+
+drop table if exists recipe;
+drop table if exists [user];
+
+create table [user] (
+    userId varchar(255) not null unique,
+    email varchar(255) not null unique,
+    firstName varchar(255) not null,
+    lastName varchar(255) not null,
+    password varchar(255) not null,
+    address varchar(255),
+    role varchar(10) not null check (role in('user', 'verifiedUser', 'admin')),
+    primary key (userId, email)
+);
+
+create table recipe (
+    recipeId varchar(255) not null unique,
+    name varchar(255) not null,
+    description varchar(255) not null,
+    authorId varchar(255) not null,
+    foreign key (authorId) references [user](userId)
+    on delete cascade,
+    pictureURL varchar(255) not null,
+    time int not null,
+    primary key (recipeId)
+    /* 'amount' int not null, */
+);
+
+create table ingredient (
+    recipeId varchar(255) not null,
+    foreign key (recipeId) references recipe(recipeId)
+    on delete cascade,
+    name varchar(255) not null,
+    amount int not null,
+    unit varchar(255) not null,
+    primary key (recipeId, name)
+);
+
+create table instruction (
+    recipeId varchar(255) not null,
+    foreign key (recipeId) references recipe(recipeId)
+    on delete cascade,
+    step int not null,
+    description varchar(255) not null,
+    primary key (recipeId, step)
+);
+
+create table list (
+    userId varchar(255) not null,
+    foreign key (userId) references "user"(userId),
+    recipeId varchar(255) not null,
+    foreign key (recipeId) references recipe(recipeId),
+    isLiked bit not null
+);
+
+create table bambooSession (
+    sessionId varchar(255) not null unique,
+    hostId varchar(255) not null,
+    foreign key (hostId) references "user"(userId),
+    address varchar(255) not null,
+    recipeId VARCHAR(255) not null,
+    foreign key (recipeId) references recipe(recipeId),
+    description varchar(255) not null,
+    dateTime datetime not null,
+    slotsNumber int not null,
+    primary key (sessionId)
+);
+
+create table bambooSessionUser (
+    sessionId varchar(255) not null,
+    foreign key (sessionId) references bambooSession(sessionId)
+    on delete cascade,
+    userId varchar(255) not null,
+    foreign key (userId) references "user"(userId)
+    on delete cascade,
+    primary key (sessionId, userId)
+);
