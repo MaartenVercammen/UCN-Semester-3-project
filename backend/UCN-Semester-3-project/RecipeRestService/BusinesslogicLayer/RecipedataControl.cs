c1285b5135cd4863913df13408e3937dc3c152c7
MaartenVercammen <vercammen.maarten.2000@gmail.com>
2022-11-10 12:35:25 +0100

    base rest files

---
+++ backend/UCN-Semester-3-project/RecipeRestService/BusinesslogicLayer/RecipedataControl.cs
@@ -0,0 +1,13 @@
+﻿using RecipesData.DatabaseLayer;
+
+namespace RecipeRestService.BusinesslogicLayer
+{
+    public class RecipedataControl : IRecipeData
+    {
+        IRecipeData _RecipeAccess;
+        public RecipedataControl(IConfiguration inConfiguration)
+        {
+            _RecipeAccess = new RecipeDatabaseAccess(inConfiguration);
+        }
+    }
+}
--- backend/UCN-Semester-3-project/RecipesData/DatabaseLayer/RecipeDatabaseAccess.cs
+++ backend/UCN-Semester-3-project/RecipesData/DatabaseLayer/RecipeDatabaseAccess.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,13 @@ using System.Threading.Tasks;
 
 namespace RecipesData.DatabaseLayer
 {
-    internal class RecipeDatabaseAccess
+    public class RecipeDatabaseAccess : IRecipeAccess
     {
+        readonly string _connectionString;
+
+        public RecipeDatabaseAccess(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("UcnConnection");
+        }
     }
 }
