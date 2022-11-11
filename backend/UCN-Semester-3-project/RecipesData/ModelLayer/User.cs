984c98b6d3ced13fe3f2b0fd235bd39efd0c6086
Tomáš Sabol <tomassabol2@gmail.com>
2022-11-11 10:15:35 +0100

    user model class

---
+++ backend/UCN-Semester-3-project/RecipesData/ModelLayer/User.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipesData.ModelLayer
+{
+    public enum Role 
+    {
+        USER,
+        VERIFIEDUSER,
+        ADMIN
+    }
+    public class User
+    {
+        public Guid UserId { get; set; }
+        public string Email { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Password { get; set; }
+        public string Address { get; set; }
+        public Role Role { get; set; }
+
+        public User(Guid userId, string email, string firstName, string lastName, string password, string address, Role role)
+        {
+            this.UserId = userId;
+            this.Email = email;
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Password = password;
+            this.Address = address;
+            this.Role = role;
+        }
+
+        public User()
+        {
+        }
+
+    }
+}
