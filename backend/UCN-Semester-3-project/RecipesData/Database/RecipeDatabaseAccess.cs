f36039b951c9f8c739fbace656c0032016f47692
ttommmy <93518302+ttommmy@users.noreply.github.com>
2022-11-11 14:02:39 +0100

    Tests are working, and so does the code

--- backend/UCN-Semester-3-project/RecipesData/Database/RecipeDatabaseAccess.cs
+++ backend/UCN-Semester-3-project/RecipesData/Database/RecipeDatabaseAccess.cs
@@ -28,9 +28,8 @@ namespace RecipesData.Database
         /// </summary>
         Recipe IRecipeAccess.GetRecipeById(Guid id)
         {
+            String guidString = id.ToString();
             Recipe recipe = new Recipe();
-            try
-            {
 
                 using (SqlConnection connection = new SqlConnection(_connectionString))
                 {
@@ -38,17 +37,14 @@ namespace RecipesData.Database
                     using (SqlCommand command = connection.CreateCommand())
                     {
                         command.CommandText = "SELECT * FROM recipe WHERE recipeId = @id";
-                        command.Parameters.AddWithValue("@id", id);
+                        command.Parameters.AddWithValue("@id", guidString);
 
                         SqlDataReader reader = command.ExecuteReader();
                         recipe = BuildObject(reader);
+                        reader.Close();
                     }
                 }
-            }
-            catch (SqlException e)
-            {
-                throw new Exception("Error while getting recipe by id", e);
-            }
+            
             return recipe;
         }
         /// <summary>
@@ -68,6 +64,7 @@ namespace RecipesData.Database
 
                         SqlDataReader reader = command.ExecuteReader();
                         recipes = BuildObjects(reader);
+                        reader.Close();
                     }
                 }
             }
@@ -99,18 +96,14 @@ namespace RecipesData.Database
         private Recipe BuildObject(SqlDataReader reader)
         {
             Recipe recipe = new Recipe();
-            try
+            while (reader.Read())
             {
-                recipe.RecipeId = new Guid((byte[])reader.GetValue(reader.GetOrdinal("recipeId")));
-                recipe.Name = reader.GetString(reader.GetOrdinal("recipeName"));
-                recipe.Description = reader.GetString(reader.GetOrdinal("recipeDescription"));
-                recipe.PictureURL = reader.GetString(reader.GetOrdinal("recipePictureUrl"));
+                recipe.RecipeId = Guid.Parse(reader.GetString(reader.GetOrdinal("recipeId")));
+                recipe.Name = reader.GetString(reader.GetOrdinal("name"));
+                recipe.Description = reader.GetString(reader.GetOrdinal("description"));
+                recipe.PictureURL = reader.GetString(reader.GetOrdinal("pictureURL"));
                 recipe.Time = reader.GetInt32(reader.GetOrdinal("time"));
                 recipe.PortionNum = reader.GetInt32(reader.GetOrdinal("portionNum"));
-            }
-            catch (SqlException e)
-            {
-                throw new Exception("Error while mapping recipe", e);
             }
             return recipe;
         }
