fd5b58699f211073daaec7f34fa581d09dc1e6c4
Tomáš Sabol <tomassabol2@gmail.com>
2022-11-14 23:24:53 +0100

    frontend done. only needs css

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,20 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Index from './component';
+import CreateRecipe from './component/recipe/CreateRecipe';
+import GetRecipe from './components/recipe/GetRecipe';
+import GetRecipes from './components/recipe/GetRecipes';
 import './css/index.css';
 
-const App:React.FC = () => {
+const App: React.FC = () => {
   return (
     <Routes>
-      <Route
-        path="/"
-        element={(
-          <Index/>
-  )}
-      />
+      <Route path="/" element={<Index />} />
+      <Route path="/createRecipe" element={<CreateRecipe />} />
+      <Route path="/recipes" element={<GetRecipes />} />
+      <Route path="/recipes/:id" element={<GetRecipe />} />
     </Routes>
   );
-}
+};
 
 export default App;
