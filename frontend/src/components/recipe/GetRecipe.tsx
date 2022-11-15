99a0cf201eba2881152a99eb2289271e2a3c35f3
Tomáš Sabol <tomassabol2@gmail.com>
2022-11-15 10:15:22 +0100

    css for single getRecipe

--- frontend/src/components/recipe/GetRecipe.tsx
+++ frontend/src/components/recipe/GetRecipe.tsx
@@ -1,43 +1,58 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import RecipeService from '../../service/recipeService';
-import { Recipe } from '../../types';
+import { Ingredient, Instruction, Recipe } from '../../types';
+import style from './GetRecipe.module.css';
 
 const GetRecipe: React.FC = () => {
+  const [recipe, setRecipe] = useState<any>([]);
+  const [ingredients, setIngredients] = useState<any>([]);
+  const [instructions, setInstructions] = useState<any>([]);
 
-    const [recipe, setRecipe] = useState<any>([]);
+  const getSingleData = async () => {
+    const response = await RecipeService.getRecipe(window.location.pathname.split('/')[2]);
+    const data = response.data;
+    setRecipe(data);
+    setIngredients(data.ingredients);
+    setInstructions(data.instructions);
+  };
 
-    const getData = async () => {
-        const response = await RecipeService.getRecipes();
-        const data = response.data;
+  useEffect(() => {
+    getSingleData();
+  }, []);
 
-        data.forEach((recipe: Recipe) => {
-            //setRecipes((recipes: any) => [...recipes, recipe]);
-        });
-    }
-
-    const getSingleData = async () => {
-        const response = await RecipeService.getRecipe(window.location.pathname.split('/')[2]);
-        const data = response.data;
-        setRecipe(data);
-    }
-
-    useEffect(() => {
-        getSingleData();
-    }, []);
-
-
-
-    return (
-        <div>
-            <img src={recipe.pictureURL} alt="" />
-            <div>
-                <h2>{recipe.name}</h2>
-                <h4>{recipe.time}</h4>
-                <h4>{recipe.description}</h4>
+  return (
+    <div>
+      <div className={style.recipeImg}>
+        <img src={recipe.pictureURL} alt="" />
+      </div>
+      <div className={style.container}>
+        <h2>{recipe.name}</h2>
+        <h5>Preparation time: {recipe.time}min</h5>
+        <p>{recipe.description}</p>
+        <h5>published by: author</h5> {/** TODO: change this to author.name  */}
+        <h5>Number of portions: {recipe.portionNum}</h5>
+        <h3>Ingredients:</h3>
+        {ingredients.map((ingredient: Ingredient) => (
+          <div key={ingredient.name}>
+            <div className={style.ingredientContainer}>
+              <p className={style.ingredientChild}>{ingredient.name}:</p>
+              <p className={style.ingredientChild}>
+                {ingredient.amount}
+                {ingredient.unit}
+              </p>
             </div>
-        </div>
-    );
+          </div>
+        ))}
+        {instructions.map((instruction: Instruction) => (
+          <div className={style.instruction} key={instruction.step}>
+            <h4>Step {instruction.step}:</h4>
+            <p>{instruction.description}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default GetRecipe;
