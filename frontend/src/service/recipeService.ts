03aa6fb1a0e7cad5f8c81cb860e5f9c710ace261
Tomáš Sabol <tomassabol2@gmail.com>
2022-11-21 15:13:25 +0100

    forgot this file oopsie

--- frontend/src/service/recipeService.ts
+++ frontend/src/service/recipeService.ts
@@ -8,22 +8,16 @@ const createRecipe = (recipe: Recipe) => instance.post('/Recipes', JSON.stringif
 const getRecipe = (recipeId: string) => instance.get<Recipe>('/Recipes/' + recipeId);
 const getRecipes = () => instance.get<Recipe[]>('/Recipes');
 const getRandomRecipe = () => instance.get<Recipe>('/Random');
-<<<<<<< HEAD
 const swipeRecipe = (swipedRecipe: SwipedRecipe) => instance.post('/SwipedRecipe', JSON.stringify(swipedRecipe));
-=======
 const deleteRecipe = (recipeId: string) => instance.delete('/Recipes/' + recipeId);
->>>>>>> dev
 
 const RecipeService = {
   createRecipe,
   getRecipe,
   getRecipes,
   getRandomRecipe,
-<<<<<<< HEAD
   swipeRecipe,
-=======
   deleteRecipe
->>>>>>> dev
 };
 
 export default RecipeService;
