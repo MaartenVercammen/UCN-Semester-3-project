de70b65f9ddcb3a4925f2bbbe9ddbc2fe7ad5724
Tomáš Sabol <tomassabol2@gmail.com>
2022-11-22 17:57:43 +0100

    making it fancy wip

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Index from './components';
+import Explore from './components/pages/Explore';
 import Home from './components/pages/Home';
+import Liked from './components/pages/Liked';
+import Recipe from './components/pages/Recipe';
 import CreateRecipe from './components/recipe/CreateRecipe';
-import GetRecipe from './components/recipe/GetRecipe';
 import GetRecipes from './components/recipe/GetRecipes';
-import Swipe from './components/swipe/swipe';
 import './css/index.css';
 
 const App: React.FC = () => {
@@ -16,7 +17,9 @@ const App: React.FC = () => {
       <Route path="/createRecipe" element={<CreateRecipe />} />
       <Route path="/createRecipe" element={<CreateRecipe />} />
       <Route path="/recipes" element={<GetRecipes />} />
-      <Route path="/recipes/:id" element={<GetRecipe />} />
+      <Route path="/recipes/:id" element={<Recipe />} />
+      <Route path="/explore" element={<Explore />} />
+      <Route path="/user/:id/liked" element={<Liked />} />
     </Routes>
   );
 };
--- frontend/src/components/pages/Navbar.tsx
+++ frontend/src/components/pages/Navbar.tsx
@@ -3,43 +3,54 @@ import {
   faMagnifyingGlass,
   faPlus,
   faUser,
-  faBars
+  faHeart
 } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React from 'react';
+import userEvent from '@testing-library/user-event';
+import React, { useEffect, useState } from 'react';
 import style from './Navbar.module.css';
 
 const Navbar: React.FC = () => {
+  const [user, setUser] = useState<any>([]);
+
+  useEffect(() => {
+    // TODO: get signed in user
+    user.userId = '00000000-0000-0000-0000-000000000000';
+    setUser(user);
+  }, []);
+
   return (
-    <nav className={style.navbar}>
-      <ul className={style.navbarParent}>
-        <li className={style.navbarChild}>
-          <a href="/app">
-            <FontAwesomeIcon icon={faHouse} className={style.icon} />
-          </a>
-        </li>
-        <li className={style.navbarChild}>
-          <a href="/app">
-            <FontAwesomeIcon icon={faMagnifyingGlass} className={style.icon} />
-          </a>
-        </li>
-        <li className={style.navbarChild}>
-          <a href="/createrecipe">
-            <FontAwesomeIcon icon={faPlus} className={style.icon} />
-          </a>
-        </li>
-        <li className={style.navbarChild}>
-          <a href="/app">
-            <FontAwesomeIcon icon={faUser} className={style.icon} />
-          </a>
-        </li>
-        <li className={style.navbarChild}>
-          <a href="/recipes">
-            <FontAwesomeIcon icon={faBars} className={style.icon} />
-          </a>
-        </li>
-      </ul>
-    </nav>
+    <>
+      <nav className={style.navbar}>
+        <ul className={style.navbarParent}>
+          <li className={style.navbarChild}>
+            <a href="/app">
+              <FontAwesomeIcon icon={faHouse} className={style.icon} />
+            </a>
+          </li>
+          <li className={style.navbarChild}>
+            <a href="/explore">
+              <FontAwesomeIcon icon={faMagnifyingGlass} className={style.icon} />
+            </a>
+          </li>
+          <li className={style.navbarChild}>
+            <a href="/createrecipe">
+              <FontAwesomeIcon icon={faPlus} className={style.icon} />
+            </a>
+          </li>
+          <li className={style.navbarChild}>
+            <a href="/app">
+              <FontAwesomeIcon icon={faUser} className={style.icon} />
+            </a>
+          </li>
+          <li className={style.navbarChild}>
+            <a href={"/user/"+ user.userId + "/liked"}>
+              <FontAwesomeIcon icon={faHeart} className={style.icon} />
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </>
   );
 };
 
