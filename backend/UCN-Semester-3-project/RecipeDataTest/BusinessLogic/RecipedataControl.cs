8fdd9f8aea2c6aca96fa6a4a6f702ceae64cd659
MaartenVercammen <vercammen.maarten.2000@gmail.com>
2022-11-24 12:58:45 +0100

    Fix naming conventions

--- backend/UCN-Semester-3-project/RecipeDataTest/BusinessLogic/RecipedataControl.cs
+++ backend/UCN-Semester-3-project/RecipeDataTest/BusinessLogic/RecipedataControl.cs
@@ -1,81 +1,44 @@
+using System.Diagnostics;
 using Moq;
 using RecipeRestService.Businesslogic;
-using Xunit.Abstractions;
 using RecipesData.Database;
 using RecipesData.Model;
+using Xunit.Abstractions;
 
-namespace RecipeDataTest
+namespace RecipeDataTest.BusinessLogic
 {
-    public class RecipedataControlTest
+    public class RecipeDataControlTest
     {
 
-        private readonly ITestOutputHelper extraOutput;
+        private readonly ITestOutputHelper _extraOutput;
         private readonly RecipedataControl _sut;
         private readonly Mock<IRecipeAccess> _acces = new Mock<IRecipeAccess>();
 
         // Valid object
-        private readonly User validUser;
-        private readonly Ingredient validIngredient;
-        private readonly Instruction validInstruction;
-        private readonly Recipe validRecipe;
-        
-        
-        // Invalid objects
-        private readonly Ingredient invalidNameIngredient;
-        private readonly Ingredient invalidamountIngredient;
-
-        private readonly Instruction invalidDescriptionInstruction;
-        private readonly Instruction invalidStepInstruction;
-        private readonly Instruction invalidStapZeroInstruction;
-
-        private readonly Recipe invalidNameRecipe;
-        private readonly Recipe invalidDescriptionRecipe;
-        private readonly Recipe invalidpictureUrlRecipe;
-        private readonly Recipe invalidTimeRecipe;
-        private readonly Recipe invalidPortionNumberRecipe;
-        private readonly Recipe invalidAuthorRecipe;
+        private readonly Recipe _validRecipe;
 
 
-        public RecipedataControlTest(ITestOutputHelper output)
+        public RecipeDataControlTest(ITestOutputHelper output)
         {
-            extraOutput = output;
+            _extraOutput = output;
             _sut = new RecipedataControl(_acces.Object);
             // Valid object
-            validUser = new User(Guid.Parse("00000000-0000-0000-0000-000000000000"),  "mail", "mark", "mark", "pass",
+            var validUser = new User(Guid.Parse("00000000-0000-0000-0000-000000000000"),  "mail", "mark", "mark", "pass",
                 "street", Role.USER);
-            validIngredient = new Ingredient("banana", 5, "kg");
-            validInstruction = new Instruction(1, "peel the banana");
-            validRecipe = new Recipe( "Bananabread", "best bananabread in the world",
+            var validIngredient = new Ingredient("banana", 5, "kg");
+            var validInstruction = new Instruction(1, "peel the banana");
+            _validRecipe = new Recipe( "Banana-bread", "best banana bread in the world",
                 "http://picture.png", 30, 4, validUser);
-
-            // Invalid objects
-            invalidNameIngredient = new Ingredient("", 5, "kg");
-            invalidamountIngredient = new Ingredient("banana", -5, "kg");
-
-            invalidDescriptionInstruction = new Instruction(1, "");
-            invalidStepInstruction = new Instruction(-1, "");
-            invalidStapZeroInstruction = new Instruction(0, "");
-
-            invalidNameRecipe = new Recipe("", "best bananabread in the world", "http://picture.png",
-                30, 4, validUser);
-            invalidDescriptionRecipe =
-                new Recipe("Bananabread", "", "http://picture.png", 30, 4, validUser);
-            invalidpictureUrlRecipe = new Recipe( "Bananabread", "best bananabread in the world", "", 30,
-                4, validUser);
-            invalidTimeRecipe = new Recipe( "Bananabread", "best bananabread in the world",
-                "http://picture.png", -10, 4, validUser);
-            invalidPortionNumberRecipe = new Recipe( "Bananabread", "best bananabread in the world",
-                "http://picture.png", 30, -5, validUser);
-            invalidAuthorRecipe = new Recipe( "Bananabread", "best bananabread in the world",
-                "http://picture.png", 30, 4, null);
+            _validRecipe.Ingredients.Add(validIngredient);
+            _validRecipe.Instructions.Add(validInstruction);
         }
 
         [Fact]
-        public void Get_WhenGivenValidId_ReturnsValidRecipe()
+        public void Get_WhenGivenId_ReturnsRecipe()
         {
             //Arrange
             Guid id = new Guid();
-            Recipe inrecipe = validRecipe;
+            Recipe inrecipe = _validRecipe;
             inrecipe.RecipeId = id;
             _acces.Setup(x => x.GetRecipeById(id))
                 .Returns(inrecipe);
@@ -84,18 +47,19 @@ namespace RecipeDataTest
             var recipe = _sut.Get(id);
 
             //Assert
+            Assert.NotNull(recipe);
             Assert.Equal(id, recipe.RecipeId);
         }
 
         [Fact]
-        public void Get_WhengivenIdAndIdDoesntExist_ReturnsNull()
+        public void Get_WhengivenIdAndExceptionIsThrown_ReturnsIdAsNull()
         {
             //Arrange
             Guid id = Guid.Empty;
             _acces.Setup(x => x.GetRecipeById(id))
                 .Throws(new Exception());
             //Act
-            Recipe recipe = _sut.Get(id);
+            Recipe? recipe = _sut.Get(id);
             //Assert
             Assert.Null(recipe);
         }
@@ -104,73 +68,74 @@ namespace RecipeDataTest
         public void Get_WhenGivenNoId_ReturnsListOfRecipes()
         {
             //Arrange
-            List<Recipe> inrecipes = new List<Recipe>()
+            List<Recipe> inRecipes = new List<Recipe>()
             {
-                validRecipe,
-                validRecipe,
-                validRecipe,
-                validRecipe
+                _validRecipe,
+                _validRecipe,
+                _validRecipe,
+                _validRecipe
             };
             _acces.Setup(x => x.GetRecipesSimplified())
-                .Returns(inrecipes);
+                .Returns(inRecipes);
             
             //Act
-            var recipes = _sut.Get();
+            List<Recipe>? recipes = _sut.Get();
 
             //Assert
-            Assert.Equal(inrecipes.Count, recipes.Count);
+            Assert.NotNull(recipes);
+            Assert.Equal(inRecipes.Count, recipes.Count);
         }
 
         [Fact]
-        public void Get_WhenGivenNoIdAndNullReturn_ReturnEmptyList()
+        public void Get_WhenGivenNoIdAndExceptionIsThrown_ReturnEmptyList()
         {
             //Arrange
             _acces.Setup(x => x.GetRecipesSimplified())
                 .Throws(new Exception());
             
             //Act
-            List<Recipe> recipes = _sut.Get();
+            List<Recipe>? recipes = _sut.Get();
             //Assert
             Assert.Null(recipes);
         }
 
         [Fact]
-        public void Delete_WhenGivenValidId_ReturnTrue()
+        public void Delete_WhenGivenId_ReturnTrue()
         {
             //Arrange
             Guid id = Guid.NewGuid();
             _acces.Setup(x => x.DeleteRecipe(id))
                 .Returns(true);
             //Act
-            bool IsDone = _sut.Delete(id);
+            bool isDone = _sut.Delete(id);
             //Assert
-            Assert.True(IsDone);
+            Assert.True(isDone);
         }
         
         [Fact]
-        public void Delete_WhenGivenInValidId_ReturnFalse()
+        public void Delete_WhenGivenNotExistingId_ReturnFalse()
         {
             //Arrange
             Guid id = Guid.NewGuid();
             _acces.Setup(x => x.DeleteRecipe(id))
                 .Returns(false);
             //Act
-            bool IsDone = _sut.Delete(id);
+            bool isDone = _sut.Delete(id);
             //Assert
-            Assert.False(IsDone);
+            Assert.False(isDone);
         }
         
         [Fact]
-        public void Delete_WhenGivenInValidIdAndErrorOccurse_ReturnFalse()
+        public void Delete_WhenGivenIdAndExceptionIsThrown_ReturnFalse()
         {
             //Arrange
             Guid id = Guid.NewGuid();
             _acces.Setup(x => x.DeleteRecipe(id))
                 .Throws(new Exception());
             //Act
-            bool IsDone = _sut.Delete(id);
+            bool isDone = _sut.Delete(id);
             //Assert
-            Assert.False(IsDone);
+            Assert.False(isDone);
         }
 
         [Fact]
@@ -178,7 +143,7 @@ namespace RecipeDataTest
         {
             //Arrange
             Guid id = Guid.NewGuid();
-            Recipe inRecipe = validRecipe;
+            Recipe inRecipe = _validRecipe;
             inRecipe.RecipeId = id;
             _acces.Setup(x => x.CreateRecipe(inRecipe))
                 .Returns(inRecipe.RecipeId);
@@ -190,11 +155,11 @@ namespace RecipeDataTest
         }
         
         [Fact]
-        public void Add_WhenInValidRecipe_ReturnsEmptyGuid()
+        public void Add_WhenRecipeAndExceptionIsThrown_ReturnsEmptyGuid()
         {
             //Arrange
             Guid id = Guid.NewGuid();
-            Recipe inRecipe = validRecipe;
+            Recipe inRecipe = _validRecipe;
             inRecipe.RecipeId = id;
             _acces.Setup(x => x.CreateRecipe(inRecipe))
                 .Throws(new Exception());
@@ -210,7 +175,7 @@ namespace RecipeDataTest
         {
             //Arrange
             Guid id = Guid.NewGuid();
-            Recipe inrecipe = validRecipe;
+            Recipe inrecipe = _validRecipe;
             inrecipe.RecipeId = id;
             List<Recipe> recipes = new List<Recipe>()
             {
@@ -222,7 +187,7 @@ namespace RecipeDataTest
             _acces.Setup(x => x.GetRandomRecipe(id))
                 .Returns(recipes);
             _acces.Setup(x => x.GetRecipeById(id))
-                .Returns(validRecipe);
+                .Returns(_validRecipe);
             //Act
             Recipe recipe = _sut.GetRandomRecipe(id);
             //Assert
@@ -230,20 +195,20 @@ namespace RecipeDataTest
         }
 
         [Fact]
-        public void GetRandomRecipe_WhenErrorAtGetRandom_ReturnsError()
+        public void GetRandomRecipe_WhenGivenIdAndExceptionIsThrown_ReturnsNull()
         {
             //Arrange
             Guid id = Guid.NewGuid();
             List<Recipe> recipes = new List<Recipe>()
             {
-                validRecipe,
-                validRecipe,
-                validRecipe
+                _validRecipe,
+                _validRecipe,
+                _validRecipe
             };
             _acces.Setup(x => x.GetRandomRecipe(id))
                 .Throws(new Exception());
             _acces.Setup(x => x.GetRecipeById(id))
-                .Returns(validRecipe);
+                .Returns(_validRecipe);
             //Act
             Recipe recipe = _sut.GetRandomRecipe(id);
             //Assert
