c04dd92b002427485c5c8546ae026b6743472030
MaartenVercammen <vercammen.maarten.2000@gmail.com>
2022-12-12 19:11:29 +0100

    Crud User service

--- adminClient/Services/UserService.cs
+++ adminClient/Services/UserService.cs
@@ -7,6 +7,8 @@ using System.Text;
 using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 using admin_client.MVVM.Model;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace admin_client.Services
 {
@@ -38,6 +40,39 @@ namespace admin_client.Services
                 user = await response.Content.ReadFromJsonAsync<User>();
             }
             return user;
+        }
+
+        public async Task<bool> DeleteUser(string id)
+        {
+            bool IsDone = false;
+            var response = await _client.DeleteAsync($"https://localhost:7088/User/{id}");
+            if (response.IsSuccessStatusCode)
+            {
+                IsDone = bool.Parse(await response.Content.ReadAsStringAsync());
+            }
+            return IsDone;
+        }
+
+        public async Task<bool> CreateUser(User user)
+        {
+            bool IsDone = false;
+            var response = await _client.PostAsJsonAsync($"https://localhost:7088/User", JsonSerializer.Serialize(user));
+            if (response.IsSuccessStatusCode)
+            {
+                IsDone = bool.Parse(await response.Content.ReadAsStringAsync());
+            }
+            return IsDone;
+        }
+
+        public async Task<bool> UpdateUser(User user)
+        {
+            bool IsDone = false;
+            var response = await _client.PutAsJsonAsync($"https://localhost:7088/User", JsonSerializer.Serialize(user));
+            if (response.IsSuccessStatusCode)
+            {
+                IsDone = bool.Parse(await response.Content.ReadAsStringAsync());
+            }
+            return IsDone;
         }
     }
 }
